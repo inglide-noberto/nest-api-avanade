0abfcfdeba65d87f38184472ede6ae8e64c910cd
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-25 13:21:00 -0300

    Past auth structure created successfully

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,10 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  getLogin(): string {
-    return 'Aqui virá o login!';
-  }
-  getRegister(): string {
-    return 'Aqui virá o cadastro!';
-  }
 }
