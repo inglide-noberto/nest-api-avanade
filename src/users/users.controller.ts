11717e41bdcdc0e83ff208a6e593b5e7ad3c23e1
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-26 14:45:11 -0300

    installing class-validator class-transformer
    created folders and dto files from crud operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Post, 
+    Patch, 
+    Delete, 
+    Body, 
+    Param,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/createUser.dto';
+import { UpdateUserDTO } from './dto/updateUser.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService){}
+
+    // criar
+    @Post()
+    async createUser(@Body() req: CreateUserDTO): Promise<string>{
+        return this.usersService.createUser(req);
+    }
+
+    // listar todos
+    @Get()
+    async findAll(){
+        this.usersService.findAll();
+    }
+    //  listar um
+    @Get(':id')
+    async findOne(@Param('id') id: string){
+        this.usersService.findOne(id);
+    }
+    // atualizar 
+    @Patch(':id')
+    async update(@Param('id') id: string, @Body() req: UpdateUserDTO){
+        return this.usersService.update(id, req);
+    }
+    // Deletar
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    async createUser(req): Promise<string> {
+        return 'Usuário cadastrado com sucessso!'
+    }
+    async findAll() {
+        return 'Lista de usuários';
+    }
+    async findOne(id: string) {
+        return `Usuário ${id}`;
+    }
+    async update(id: string , req) {
+        return `Usuário ${id} atualizado com sucesso!`;
+    }
+
+}
