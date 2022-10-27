b92e4d536d3fdee3f055c3b3a5eae196ac599d1b
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-27 14:22:47 -0300

    connecting the bank with the application
    adding typeorm folder and configuring package.json

---
+++ typeorm/migration/1666891234240-users.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class users1666891234240 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
